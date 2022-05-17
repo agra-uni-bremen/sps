823212d9aa53ab5610b1de655573e84d618fa3ba
Sören Tempel <tempel@uni-bremen.de>
2022-05-17 19:23:43 +0200

    util: add custom switch macro

--- lib/util.scm
+++ lib/util.scm
@@ -34,3 +34,53 @@
 
 (: dec (number -> number))
 (define (dec n) (- n 1))
+
+;; This is similar to case, but the keys are evaluated & equal? is the test.
+
+; From moremacros by Kon Lovett.
+; Licensed under MIT.
+;
+; See https://wiki.call-cc.org/eggref/5/moremacros#license
+(define-syntax %switch
+  (er-macro-transformer
+    (lambda (frm ren cmp)
+      (##sys#check-syntax 'switch frm '(_ _ . _))
+      (let ((exp (cadr frm))
+            (body (cddr frm))
+            (_tmp (ren 'tmp))
+            (_else (ren 'else))
+            (_or (ren 'or)) )
+        `(let ((,_tmp ,exp))
+          ,(let expd-form ((clauses body) (seen-else #f))
+            (cond
+              ((null? clauses)
+                '(void) )
+              ((not (pair? clauses))
+                (syntax-error 'switch "invalid syntax" clauses) )
+              (else
+                (let ((clause (car clauses))
+                      (rclauses (cdr clauses)) )
+                  (##sys#check-syntax 'switch clause '#(_ 1))
+                  (cond
+                    ((cmp _else (car clause))
+                      (expd-form rclauses #t)
+                      `(begin ,@(cdr clause)) )
+                    (seen-else
+                      (##sys#notice
+                        "non-`else' clause following `else' clause in `switch'"
+                        (strip-syntax clause))
+                      (expd-form rclauses #t)
+                      '(begin) )
+                  (else
+                    `(if
+                      (,_or
+                        ,@(map (lambda (x) `(equal? ,_tmp ,x))
+                        (car clause)))
+                      (##core#begin
+                        ,@(cdr clause))
+                        ,(expd-form rclauses #f) ) ) ) ) ) ) ) ) ) ) ) )
+
+(define-syntax switch
+  (syntax-rules ()
+    ((switch ARG BODY ...)
+     (%switch ARG BODY ... (else (error "no match"))))))
