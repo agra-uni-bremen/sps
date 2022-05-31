3930ae52c8c0ace08d1f1e72af3bb8c906a707de
Sören Tempel <tempel@uni-bremen.de>
2022-05-31 04:57:26 +0200

    protocol: Hackfix for not hardcoding state machine name

--- lib/protocol.scm
+++ lib/protocol.scm
@@ -1,7 +1,7 @@
 (define DATA  #x00)
 (define RESET #x01)
 
-(define (handle-conn sm input-port output-port)
+(define (handle-conn sm-name sm input-port output-port)
   (let ((msg (read-bencode input-port)))
     (when msg ;; not eof
       (when (or (not (vector? msg)) (not (eq? (vector-length msg) 2)))
@@ -11,8 +11,8 @@
             (msg-content (vector-ref msg 1)))
         (switch msg-type
           ((RESET)
-           (state-machine-reset! 'mqtt-machine) ;; TODO
-           (handle-conn sm input-port output-port))
+           (state-machine-reset! sm-name)
+           (handle-conn sm-name sm input-port output-port))
 
           ((DATA)
            (unless (string? msg-content)
@@ -22,9 +22,9 @@
                          ((resp has-next?) (state-machine-run sm bv)))
              (write-format resp output-port)
              (when has-next?
-               (handle-conn sm input-port output-port)))))))))
+               (handle-conn sm-name sm input-port output-port)))))))))
 
-(define (sm-server sm host port)
+(define (%sm-server sm-name sm host port)
   ;; Default backlog according to tcp-listen documentation.
   (define backlog 100)
 
@@ -32,12 +32,20 @@
     (loop
       (let*-values (((in out) (tcp-accept listener)))
         ;; Reset state machine everytime the client reconnects.
-        (state-machine-reset! 'mqtt-machine)
+        (state-machine-reset! sm-name)
 
         ;; Disable read and write timeouts (default: 1 min).
         (parameterize ((tcp-read-timeout #f)
                        (tcp-write-timeout #f))
-          (handle-conn sm in out))
+          (handle-conn sm-name sm in out))
 
         (close-input-port in)
         (close-output-port out)))))
+
+;; Hack to not require passing state machine name separately
+;; TODO: Don't expand define-state-machine directly into a procedure.
+
+(define-syntax sm-server
+  (syntax-rules ()
+    ((sm-server SM host port)
+     (%sm-server (quote SM) SM host port))))
