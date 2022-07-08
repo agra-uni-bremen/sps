a6d5109d999b53c39cc2de3ac2d105f7d2d7fb60
Sören Tempel <tempel@uni-bremen.de>
2022-07-08 19:57:59 +0200

    examples: add unconstrained udp example

---
+++ examples/udp-unconstrained.scm
@@ -0,0 +1,79 @@
+(import (scheme base)
+        (scheme write)
+        (scheme process-context)
+
+        (srfi 1)
+        (srfi 151)
+
+        (sisl)
+        (sps))
+
+(define (make-response fmt)
+  (ipv6-packet
+    udp-next-header
+    (udp-datagram
+      fmt)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; IPv6 constants
+(define ipv6-version-value #x6)
+(define ipv6-loopback #u8(#x00 #x00 #x00 #x00
+                          #x00 #x00 #x00 #x00
+                          #x00 #x00 #x00 #x00
+                          #x00 #x00 #x00 #x01))
+(define ipv6-srcaddr #u8(#xfe #x80 #x00 #x00
+                         #x00 #x00 #x00 #x00
+                         #x00 #x00 #x00 #x00
+                         #x00 #x00 #x00 #x01))
+
+;; UDP constants
+(define udp-next-header 17)
+(define udp-port 1883)     ;; CONFIG_EMCUTE_DEFAULT_PORT
+(define udp-src-port 1883) ;; can essentially be anything
+
+;; MQTT constants
+(define mqtt-msg-connack #x05)
+(define mqtt-code-accept #x00)
+
+;; See https://datatracker.ietf.org/doc/html/rfc8200#section-3
+(define-input-format (ipv6-packet next-hdr &encapsulate payload)
+  (make-uint 'version 4 ipv6-version-value)
+  (make-uint 'traffic-class 8 #x0)
+  (make-uint 'flow-label 20 #x0)
+  (make-uint 'payload-length 16 (input-format-bytesize payload))
+  (make-uint 'next-header 8 next-hdr)
+  (make-uint 'hop-limit 8 #x42)
+  (make-concrete 'src-addr 128 ipv6-srcaddr)
+  (make-concrete 'dst-addr 128 ipv6-loopback))
+
+;; See https://datatracker.ietf.org/doc/html/rfc768
+(define-input-format (udp-datagram &encapsulate payload)
+  (make-uint 'src-port 16 udp-src-port)
+  (make-uint 'dst-port 16 udp-port)
+  (make-uint 'length 16 (+ 8 (input-format-bytesize payload)))
+  (make-uint 'checksum 16 0)) ;; checksum checks disabled in RIOT
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-state-machine symbolic-machine
+  (start symbolic)
+
+  (define-state (symbolic input)
+    (-> (make-response
+          (make-input-format
+            (make-symbolic
+              'mqtt-fully-symbolic
+              (bytes->bits 56))))
+        symbolic)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (main args)
+  (if (eq? (length args) 3)
+    (sm-server symbolic-machine (second args) (string->number (third args)))
+    (error "Missing host and port argument")))
+
+(cond-expand
+  ((or chicken-script compiling) (main (command-line)))
+  (else #t))
