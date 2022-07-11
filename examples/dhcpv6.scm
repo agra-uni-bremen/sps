911c6364be36e7332144ce5ad90a79957f86a07c
Sören Tempel <tempel@uni-bremen.de>
2022-07-11 03:36:29 +0200

    dhcpv6.scm: Extrat client id option

--- examples/dhcpv6.scm
+++ examples/dhcpv6.scm
@@ -68,14 +68,42 @@
       (ipv6-packet 0 (udp-datagram (dhcpv6-hdr 23 42)))
       'dhcpv6-id)))
 
+;; Extract DHCPv6 option from message.
+(define (dhcp-opt-val bv opt-code)
+ (define (%dhcp-opt-val bv)
+   (if (zero? (bytevector-length bv))
+     #f
+     (let ((code (bytevector->number (bytevector-copy bv 0 2)))
+           (len  (bytevector->number (bytevector-copy bv 2 4)))
+           (rest (bytevector-copy bv 4)))
+       (display "code: ") (display code) (newline)
+       (display "len: ") (display len) (newline)
+       (if (eqv? code opt-code)
+         (bytevector-copy rest 0 len)
+         (%dhcp-opt-val (bytevector-copy rest len))))))
+
+ (let* ((opt-size (- (bytevector-length bv)
+                     (input-format-bytesize
+                       (ipv6-packet 0 (udp-datagram (make-input-format))))))
+        (base-fmt (ipv6-packet 0 (udp-datagram (dhcpv6-hdr-opt 0 0 opt-size)))))
+   (%dhcp-opt-val (get-field bv base-fmt 'dhcpv6-opts))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; DHCPv6 constants
+;; DHCPv6 message types
 ;; See: https://datatracker.ietf.org/doc/html/rfc8415#section-7.3
 (define dhcpv6-solicit 1)
 (define dhcpv6-advertise 2)
 (define dhcpv6-request 3)
 (define dhcpv6-confirm 4)
+(define dhcpv6-renew 5)
+(define dhcpv6-rebind 6)
+(define dhcpv6-reply 7)
+
+;; DHCPv6 option types
+;; See: https://datatracker.ietf.org/doc/html/rfc8415#section-24
+(define dhcpv6-opt-client-id 1)
+(define dhcpv6-opt-server-id 2)
 
 ;; DHCPv6 header format.
 ;; Primarly intended to extract fields from client messages.
@@ -84,23 +112,41 @@
 (define-input-format (dhcpv6-hdr msg-type id)
   (make-uint 'dhcpv6-type 8 msg-type)
   (make-uint 'dhcpv6-id 24 id))
+(define-input-format (dhcpv6-hdr-opt msg-type id opt-size)
+  (make-uint 'dhcpv6-type 8 msg-type)
+  (make-uint 'dhcpv6-id 24 id)
+  (make-uint 'dhcpv6-opts (- (bytes->bits opt-size) 8 24) 0))
 
 ;; See:
 ;;  • Advertise Handling: https://datatracker.ietf.org/doc/html/rfc8415#section-16.3
 ;;  • Message Format: https://datatracker.ietf.org/doc/html/rfc8415#section-8
 ;;  • Option Format: https://datatracker.ietf.org/doc/html/rfc8415#section-21.1
-(define-input-format (dhcpv6-advertise-fmt id)
+(define-input-format (dhcpv6-advertise-fmt id client-id)
   (make-uint 'dhcpv6-type 8 dhcpv6-advertise)
   (make-uint 'dhcpv6-id 24 id)
 
   ;; Client Identifier Option
-  ;; XXX: Partially symbolic for now, could also be extracted.
   (make-uint 'optcode-client-id 16 1)
-  (make-uint 'optlen-client-id 16 4)
-  (make-symbolic 'optdata-client-id 32)
+  (make-uint 'optlen-client-id 16 (bytevector-length client-id))
+  (make-concrete 'optdata-client-id (bytes->bits (bytevector-length client-id)) client-id)
 
   ;; Server Identifier Option
-  (make-uint 'optcode-server-id 16 2)
+  (make-uint 'optcode-server-id 16 dhcpv6-opt-server-id)
+  (make-uint 'optlen-server-id 16 4)
+  (make-concrete 'optdata-server-id (bytes->bits 4) (bytevector #xAA #xBB #xCC #xDD)))
+
+;; See: https://datatracker.ietf.org/doc/html/rfc8415#section-16.10
+(define-input-format (dhcpv6-reply-fmt id client-id)
+  (make-uint 'dhcpv6-type 8 dhcpv6-reply)
+  (make-uint 'dhcpv6-id 24 id)
+
+  ;; Client Identifier Option
+  (make-uint 'optcode-client-id 16 1)
+  (make-uint 'optlen-client-id 16 (bytevector-length client-id))
+  (make-concrete 'optdata-client-id (bytes->bits (bytevector-length client-id)) client-id)
+
+  ;; Server Identifier Option
+  (make-uint 'optcode-server-id 16 dhcpv6-opt-server-id)
   (make-uint 'optlen-server-id 16 4)
   (make-concrete 'optdata-server-id (bytes->bits 4) (bytevector #xAA #xBB #xCC #xDD)))
 
@@ -111,12 +157,28 @@
 
   (define-state (pre-solicit input)
     (switch (dhcp-type input)
-      ;; TODO https://datatracker.ietf.org/doc/html/rfc8415#section-16.3
+      ;; See https://datatracker.ietf.org/doc/html/rfc8415#section-16.3
       ((dhcpv6-solicit) (-> (make-response
-                              (dhcpv6-advertise-fmt (dhcp-trans-id input)))
+                              (dhcpv6-advertise-fmt
+                                (dhcp-trans-id input)
+                                (dhcp-opt-val input dhcpv6-opt-client-id)))
                             advertised))))
 
-  (define-state advertised))
+  (define-state (advertised input)
+    (switch (dhcp-type input)
+      ((dhcpv6-request) (-> (make-response
+                              (dhcpv6-reply-fmt
+                                (dhcp-trans-id input)
+                                (dhcp-opt-val input dhcpv6-opt-client-id)))
+                            replied))))
+
+  (define-state (replied input)
+    (-> (make-response
+          (make-input-format
+            (make-symbolic
+              'mqtt-fully-symbolic
+              (bytes->bits 32))))
+        replied)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
